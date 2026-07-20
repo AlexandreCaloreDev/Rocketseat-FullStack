753a2744eb26b480f463af5f3a1753103ced3d04
AlexandreCaloreDev <caloredev@gmail.com>
2026-07-20 20:22:22 -0300

    Estudos do dia - update | Delete e seed de tabelas

---
+++ Aulas/Nível 9/04-Criando uma API de restaurante/04-Mesas/01-Criando tabela de mesas/src/routes/products-routes.ts
@@ -0,0 +1,11 @@
+import { ProductController } from "@/controllers/products-controller"
+import {Router} from "express"
+
+const productsRoutes = Router()
+const productController = new ProductController
+
+productsRoutes.get("/", productController.index)
+productsRoutes.post("/", productController.create)
+productsRoutes.put("/:id", productController.update)
+productsRoutes.put("/:id", productController.remove)
+export {productsRoutes}
